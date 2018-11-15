67a6b9e59833493df22265410855d51754a04987
Weng Haoyu <whyharry9@gmail.com>
2018-11-15 16:43:07 -0500

    Create update.ps1

---
+++ update/update.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest https://raw.githubusercontent.com/zbot473/SurvivHacks/master/ChromeExtension/init.js -OutFile cache\init.js
+Invoke-WebRequest https://raw.githubusercontent.com/zbot473/SurvivHacks/master/ChromeExtension/background.js -OutFile cache\background.js
+Invoke-WebRequest https://raw.githubusercontent.com/zbot473/SurvivHacks/master/ChromeExtension/manifest.json -OutFile cache\manifest.json
+robocopy cache ..\ChromeExtension /mov
