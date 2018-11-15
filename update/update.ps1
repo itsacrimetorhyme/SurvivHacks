066571467bb8168b4bc20613b1002eb9c34e7f98
zbot473 <13140490+zbot473@users.noreply.github.com>
2018-11-15 17:45:03 -0500

    Create cache folder first
    Probably works now

--- update/update.ps1
+++ update/update.ps1
@@ -1,3 +1,4 @@
+new-item cache -ItemType directory
 Invoke-WebRequest https://raw.githubusercontent.com/zbot473/SurvivHacks/master/ChromeExtension/init.js -OutFile cache\init.js
 Invoke-WebRequest https://raw.githubusercontent.com/zbot473/SurvivHacks/master/ChromeExtension/background.js -OutFile cache\background.js
 Invoke-WebRequest https://raw.githubusercontent.com/zbot473/SurvivHacks/master/ChromeExtension/manifest.json -OutFile cache\manifest.json
